cc1e1a515c3cd1e15b6072a9aa49c92f73d2aebd
mes3n <skidmackan@gmail.com>
2023-05-13 02:48:34 +0200

    improved mobile view support

--- src/components/common/Common.tsx
+++ src/components/common/Common.tsx
@@ -1,10 +1,57 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const pageTransitions = {
+  initial: {
+    x: -400,
+    opacity: 0,
+  },
+  in: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.1 },
+  },
+  out: {
+    opacity: 0,
+    x: 1000,
+    transition: { duration: 0.2 },
+  },
+};
+
+export const RootContainer = styled.div`
   position: fixed;
 
   width: 100%;
   height: 100%;
 
+  overflow-y: scroll;
+
   background-color: #caf0f8;
+`
+
+export const Container = styled.div`
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  margin-top: 64px;  
+`
+
+export const Title = styled.div`
+  font-family: 'Sora', sans-serif;
+  font-size: 64px;
+`
+
+export const Text = styled.div`
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 24px;
+
+  text-align: center;
+`
+
+export const Margin = styled.div<{width?: number, height?: number}>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
 `
--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -9,19 +9,28 @@ const Logo = () => {
 }
 
 const Container = styled.div`
-  height: 100%;
   width: fit-content;
+  height: min-content;
 
   display: grid;
   place-items: center;
 
-  margin-top: 16px;
+  margin-top: 12px;
   margin-left: 16px;
+
+  @media screen and (max-width: 786px) {
+    margin-top: 6px;
+    margin-left: 8px;
+  }
 `
 
 const Title = styled.div`
   font-family: 'Maven Pro', sans-serif;
   font-size: 48px;
+
+  @media screen and (max-width: 786px) {
+    font-size: 24px;
+  }
 `
 
 export default Logo
